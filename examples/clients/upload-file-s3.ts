d684586f7aecb98d22ade49d6e3d88f55cef9ccd
Eetu Tuomala <eetu.tuomala@maas.global>
2021-10-24 23:38:26 +0300

    Add comment

--- examples/clients/upload-file-s3.ts
+++ examples/clients/upload-file-s3.ts
@@ -1,18 +1,18 @@
 import got from "got";
 
 (async () => {
-  // Fetch signed URL
+  // Fetch a signed URL. Pass the API URL and x api key as environmental variables
   const response = await got.get(process.env.API_URL as string, {
     headers: {
       "x-api-key": process.env.X_API_KEY as string,
     },
   });
   const { url } = JSON.parse((await response).body);
-  // Create dummy payload
+  // Create a dummy payload
   const payload = Buffer.from(
     JSON.stringify({ date: new Date().toISOString() })
   );
-  // Upload payload to S3
+  // Upload the payload to the S3 bucket
   try {
     const { statusCode } = await got.put(url, { body: payload });
     console.log("upload complete", statusCode);
